2b9294e603aa239c7c494445d6a3447df657a1e0
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-03 10:05:20 -0700

    edit index, create, and details routes in itemscontroller.cs and their corresponding view files
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,46 +1,52 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-//     private readonly ToDoListContext _db;
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     public ItemsController(ToDoListContext db)
-//     {
-//       _db = db;
-//     }
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Item> model = _db.Items.Include(items => items.Category).ToList();
-//       return View(model);
-//     }
+      public ActionResult Index()
+      {
+          return View(_db.Items.ToList());
+      }
 
-//     public ActionResult Create()
-//     {
-//       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-//       return View();
-//     }
+    public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
+    }
 
-//     [HttpPost]
-//     public ActionResult Create(Item item)
-//     {
-//       _db.Items.Add(item);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Create(Item item, int CategoryId)
+    {
+      _db.Items.Add(item);
+      if (CategoryId != 0)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult Details(int id)
-//     {
-//       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-//       return View(thisItem);
-//     }
+    public ActionResult Details(int id)
+    {
+      var thisItem = _db.Items
+      .Include(item => item.Categories)
+      .ThenInclude(join => join.Category)
+      .FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
 
 //     public ActionResult Edit(int id)
 //     {
@@ -71,5 +77,5 @@
 //       _db.SaveChanges();
 //       return RedirectToAction("Index");
 //     }
-//   }
-// }
+  }
+}
