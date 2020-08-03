88fef1cdbae9bf20e9587a774d8cb31f83c78a70
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-03 09:39:02 -0700

    initial commit
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+  public class ToDoListContext : DbContext
+  {
+    public virtual DbSet<Category> Categories { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<CategoryItem> CategoryItem { get; set; }
+
+    public ToDoListContext(DbContextOptions options) : base(options) { }
+  }
+}
